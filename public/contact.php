f6d19fb95e4cab5f135ebb0e5544c7eab08ddcaa
Sanjit Dutta <sdlynx@gmail.com>
2020-12-04 15:18:38 -0500

    updates to customize site

---
+++ public/contact.php
@@ -0,0 +1,148 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <title>SIMple SIM - Stay Connected</title>
+        <meta name="description" content="SIMple SIM is the eSIM marketplace of the future." />
+
+        <!--Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Arimo:400,400i,700,700i" rel="stylesheet">
+
+        <!--vendors styles-->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+        <!-- Bootstrap CSS / Color Scheme -->
+        <link rel="stylesheet" href="css/red.css" id="theme-color">
+    </head>
+    <body data-spy="scroll" data-target="#ebook-navbar" data-offset="0">
+
+        <!--navigation-->
+        <nav class="navbar navbar-expand-md navbar-light fixed-top sticky-navigation" id="ebook-navbar">
+            <a class="navbar-brand" href="index.html">
+                SIMple SIM
+            </a>
+            <button class="navbar-toggler navbar-toggler-right border-0" type="button" data-toggle="collapse"
+                    data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                <span data-feather="menu"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarCollapse">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link page-scroll" href="./#home">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link page-scroll" href="./#about">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link page-scroll" href="./#testimonials">Testimonials</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link page-scroll" href="./#pricing">Pricing</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link page-scroll" href="./#faq">FAQ</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link page-scroll" href="./#contact">Contact</a>
+                    </li>
+                </ul>
+                <form class="form-inline">
+                    <a class="btn btn-primary" href="#buy-navbar">
+                        Explore plans
+                    </a>
+                </form>
+            </div>
+        </nav>
+
+        <!--hero header-->
+        <section class="pt-7 pt-md-5" id="home">
+            <div class="container">
+                <div class="row vh-md-100">
+                    <div class="col-md-6 my-md-auto mb-5 mb-md-0 text-center text-md-left">
+                        <h1 class="mt-2">Well this is awkward...</h1>
+                        <p class="lead mb-4">We&apos;re actively working on getting our product ready for market. Please drop
+                          us your email to join our waitlist and we'll reach out once we&apos;re ready to go!</p>
+                        <form method="POST" action="./contact">
+                          <input class="share-email" type="text" placeholder="Email" id="email" />
+                          <button href="#" class="btn btn-primary d-inline-flex flex-row align-items-center">
+                              Submit
+                          </button>
+                        </form>
+                    </div>
+                    <div class="col-md-6 my-md-auto">
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!--footer-->
+        <footer class="py-5 bg-light" id="contact">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4">
+                        <h5>About SIMple SIM</h5>
+                        <p class="text-muted mt-4">
+                            Founded in 2020, SIMple SIM was founded by five jetsetting Wharton MBA students with a mission to
+                            offer cheap and easy data connectivity to travel aficionados all across the world.
+                        </p>
+                        <ul class="list-inline social social-sm mt-4">
+                            <li class="list-inline-item">
+                                <a href="#facebook"><i class="fa fa-facebook"></i></a>
+                            </li>
+                            <li class="list-inline-item">
+                                <a href="#twitter"><i class="fa fa-twitter"></i></a>
+                            </li>
+                            <li class="list-inline-item">
+                                <a href="#googleplus"><i class="fa fa-google-plus"></i></a>
+                            </li>
+                            <li class="list-inline-item">
+                                <a href="#linkedin"><i class="fa fa-linkedin"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-md-3 ml-auto">
+                        <h5>Useful Links</h5>
+                        <ul class="list-unstyled mt-4">
+                            <li><a href="./">About</a></li>
+                            <li><a href="#privacy">Privacy Policy</a></li>
+                            <li><a href="#tos">Terms of Service</a></li>
+                            <li><a href="#blog">Blog</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-md-3 text-center">
+                        <p class="text-white">
+                            <span data-feather="message-circle" width="40" height="40"></span>
+                        </p>
+                        <h5 class="text-capitalize">Need Help?</h5>
+                        <p class="text-muted mt-4">
+                            Send us an email at <a href="#contact-support-footer">support@simple.sim</a> if you have any qustions. We'll help you out.
+                        </p>
+                    </div>
+                </div>
+                <hr class="my-5"/>
+                <div class="row">
+                    <div class="col-12 text-muted text-center">
+                        &copy; 2020 SIMple SIM - All Rights Reserved
+                    </div>
+                </div>
+            </div>
+        </footer>
+
+        <!--scroll to top-->
+        <div class="scroll-top">
+            <i class="fa fa-angle-up" aria-hidden="true"></i>
+        </div>
+
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/feather-icons/4.7.0/feather.min.js"></script>
+        <script src="js/scripts.js"></script>
+
+        <!-- LiveReload -->
+        <script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
+    </body>
+</html>
